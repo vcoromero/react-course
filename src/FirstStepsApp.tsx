41949b9d16b61112aabce35152f5dc473c0b7e1d
Victor Romero <vcoromero@gmail.com>
2026-02-11 17:45:51 -0600

    feat: starting with props

--- src/FirstStepsApp.tsx
+++ src/FirstStepsApp.tsx
@@ -4,10 +4,10 @@ export function FirstStepsApp() {
     return (
         <>
             <h1>Shopping Cart</h1>
-            <ItemCounter />
-            <ItemCounter />
-            <ItemCounter />
-            <ItemCounter />
+            <ItemCounter productName="Nintendo Switch 2" quantity={10} />
+            <ItemCounter productName="PlayStation 5" quantity={5} />
+            <ItemCounter productName="Xbox Series X" quantity={3} />
+            <ItemCounter productName="PC" quantity={2} />
 
         </>
     )
