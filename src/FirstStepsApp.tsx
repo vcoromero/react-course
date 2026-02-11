f525b92618adae9b450ca7609d27739430e5b25e
Victor Romero <vcoromero@gmail.com>
2026-02-11 15:34:36 -0600

    feat: my first component

---
+++ src/FirstStepsApp.tsx
@@ -0,0 +1,14 @@
+export function FirstStepsApp() {
+    return (
+        <>
+            <h1>Hello World</h1>
+            <p>This is a paragraph</p>
+
+            <button>Click me</button>
+
+            <div>
+                <h2>This is a div</h2>
+            </div>
+        </>
+    )
+}
