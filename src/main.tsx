ee0f323c3802fca09757d08858706c83bf471bb4
Victor Romero <vcoromero@gmail.com>
2026-02-13 23:08:23 -0600

    feat: erasing unused files

--- src/main.tsx
+++ src/main.tsx
@@ -3,25 +3,11 @@ import { createRoot } from 'react-dom/client'
 import { Toaster } from 'sonner'
 import './index.css'
 
-import { ProfessionalApp } from './09-useContext/ProfessionalApp'
+import { GifApp } from './GifApp'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Toaster />
-    {/* <PokemonPage /> */}
-    {/* <FocusScreen /> */}
-    {/* <TasksApp/> */}
-    {/* <ScrambleWords /> */}
-    {/* <MemoHook /> */}
-    {/* <MemoCounter /> */}
-    {/* <InstagromApp /> */}
-    {/* <Suspense fallback={
-      <div className='bg-gradient flex flex-col gap-4'>
-        <h1 className='text-2xl'> cargando </h1>
-      </div>
-    }>
-      <ClientInformation getUser={getUserAction(1000)} />
-    </Suspense> */}
-    <ProfessionalApp />
+    <GifApp />
   </StrictMode>,
 )
