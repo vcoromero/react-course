dd2ecd871d1366c772d88849b5d0befef5f8ca9d
Victor Romero <vcoromero@gmail.com>
2026-02-18 00:05:55 -0600

    chore: first time using useRef

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { PokemonPage } from './03-examples/PokemonPage'
 import './index.css'
+import { FocusScreen } from './04-useRef/FocusScreen'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <PokemonPage />
+    {/* <PokemonPage /> */}
+    <FocusScreen />
   </StrictMode>,
 )
